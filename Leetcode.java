2f824a6eb204ebdc6181760023934cf19aaff4bb
Vijay Chittamuru <113068874+VijayChittamuru@users.noreply.github.com>
2023-06-26 10:28:35 +0530

    Add files via upload

---
+++ Leetcode.java
@@ -0,0 +1,580 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+
+public class Leetcode {
+
+    public static void main(String args[]) {
+        Leetcode lcTest = new Leetcode();
+
+        int[] a1 = {0, 2, 1, 1};
+        lcTest.sortColors(a1);
+        System.out.println(Arrays.toString(a1));
+    }  
+
+    // Solves the problem though doesn't technically 'sort'
+    // Still fast and constant memory
+    public void sortColors(int[] nums) {
+        int numZ = 0, numO = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            switch(nums[i]){
+                case 0: numZ++; break;
+                case 1: numO++; break;
+            }
+        }
+        for(int i = 0; i < numZ; i++)
+            nums[i] = 0;
+        
+        for(int i = numZ; i < (numZ + numO); i++)
+            nums[i] = 1;
+        
+        for(int i = numZ+numO; i < nums.length; i++)
+            nums[i] = 2;
+    }
+
+    // Not done
+    public void setZeroes(int[][] matrix) {
+        for(int i = 1; i < matrix.length; i++) {
+            for(int j = 1; j < matrix[i].length; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Check first row/column if zero change rest of row
+        // Change first ones last
+    }
+
+    // Alternate solution: return Math.pow(x, n);
+    public double myPow(double x, int n) {
+        // Recursive sol, doesn't count 0.1/0.001/etc. cases
+        /* // No problem without, but greatly speeds up edge cases
+        if(x == 1)
+            return 1;
+        if(x == 0)
+            return 0;
+
+        // Base cases
+        if(n == 0)
+            return 1;
+        if(n == 1)
+            return x;
+        if(n > 0)
+            return (x * this.myPow(x, n-1));
+        else
+            return (1/(this.myPow(x, -n))); */
+        if(x == -1) {
+            if(n % 2 == 1)
+                return -1;
+            else
+                return 1;
+        }
+        if(x == 1)
+            return 1;
+        if(x == 0)
+            return 0;
+        if(n == 0)
+            return 1;
+        if (n == 1)
+            return x;
+        
+        double mult = x;
+
+        if(n < 0){
+            x = 1/x;
+            mult = 1/mult;
+
+            for(int i = -1; i > n; i--) {
+                x*= mult;
+                if(x == 0){
+                    return 0;
+                }
+            }
+        }
+        else {
+            for(int i = 1; i < n; i++){
+                x *= mult;
+                if(x == 0)
+                    return 0;
+            }
+        }
+        return x;
+    }
+
+    // Sort array, then two pointers. O(nlogn)
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        int[] sorted = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(sorted);
+
+        int left = 0, right = nums.length - 1, sum = 0, val1 = 0, val2 = 0;
+
+        while(left < right) {
+            sum = sorted[left] + sorted[right];
+            if(sum == target) {
+                val1 = sorted[left];
+                val2 = sorted[right];
+                break;
+            }
+            else if(sum < target)
+                left++;
+            else
+                right--;
+        }
+        //System.out.printf("Left: %s, Right: %s, val1: %s, val2: %s \n", left, right, val1, val2);
+        boolean done = false;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == val1 && !done){
+                done = true;
+                ans[0] = i;
+            }
+            if(nums[i] == val2 && i != ans[0])
+                ans[1] = i;
+        }
+        return ans;
+    }
+
+    // Technically O(n), maybe more memory
+    public int[] twoSum2(int[] nums, int target){
+        int[] ans = new int[2];
+
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
+        }
+
+        int diff = 0;
+        for(int i = 0; i < nums.length; i++){
+            diff = target - nums[i];
+            if(map.get(diff) != null && map.get(diff) != i) {
+                ans[0] = map.get(diff);
+                ans[1] = i;
+                return ans;
+            } 
+        }
+
+        return ans;
+    }
+
+    // returns array of double size w all elements repeated
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2*n];
+        for(int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+        }
+        return ans;
+    }
+
+    // Removes duplicate values from a sorted linked list
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null)
+            return head;
+        
+        ListNode tracker = head;
+        while(tracker.next != null) {
+            if(tracker.val == tracker.next.val){
+                tracker.next = tracker.next.next;
+            }
+            else{
+                tracker = tracker.next;
+            }
+        }
+
+        return head;
+    }
+
+    // Adds 1 to integer where each digit is array element
+    // First element is most significant
+    public int[] plusOne(int[] digits) {
+        // carry set to 1 to account for inital plus 1
+        int carry = 1, sum;
+        for(int i = digits.length - 1; i >= 0; i--){
+            sum = digits[i] + carry;
+            digits[i] = sum % 10;
+
+            carry = sum/10;
+        }
+
+        if(carry != 0) {
+            int[] newDigits = new int[digits.length + 1];
+
+            for(int i = 0; i < digits.length + 1; i++){
+                if(i == 0)
+                    newDigits[i] = carry;
+                else
+                    newDigits[i] = digits[i-1];
+            }
+            return newDigits;
+        }
+
+        return digits;
+    }
+
+    // Is checking rows and columns, not checking sub-boxes yet
+    public boolean isValidSudoku(char[][] board){
+        // Check rows
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] != '.') {
+                    for(int k = j + 1; k < 9; k++) {
+                        if(board[i][j] == board[i][k])
+                            return false;
+                    }
+                }
+            }
+        }
+        // Check columns
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if(board[j][i] != '.') {
+                    for(int k = j + 1; k < 9; k++) {
+                        if(board[j][i] == board[k][i])
+                            return false;
+                    }
+                }
+            }
+        }
+
+        // Check boxes
+        
+        return true;
+    }
+
+    // Technically working but slow for large numbers
+    // Kind of brute force addition/subtraction
+    public int divide(int dividend, int divisor) {
+        int quotient = 0, sum = 0;
+        boolean isNeg = false;
+
+        if(dividend == Integer.MIN_VALUE) {
+            if(divisor == -1)
+                return Integer.MAX_VALUE;
+            if(divisor < 0)
+                divisor = -divisor;
+            else
+                isNeg = true;
+        }
+        else{
+            // If both are neg, then quotient should be +ve
+            if(dividend < 0) {
+                isNeg = !isNeg;
+                dividend = -dividend;
+            }
+            if(divisor < 0){
+                isNeg = !isNeg;
+                divisor = -divisor;
+           }
+        }
+        // Only if it is MIN_VALUE
+        if(dividend < 0) {
+            sum = dividend;
+            while(sum <= 0) {
+                sum += divisor;
+                quotient++;
+            }
+        }
+
+        while(sum <= dividend && sum >= 0) {
+            sum += divisor;
+            quotient++;
+        }
+
+        if(isNeg)
+            return -(quotient - 1);
+        
+        return quotient-1;
+    }
+
+    // Finds the length of the longest substring w/o repeating chars
+    // Very slow and memory inefficient, uses HashMap
+    // Redo maybe w HashSet
+    public int lengthOfLongestSubstring(String s) {
+        
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>(s.length());
+        // Store char and key in hashmap, if dupe found, start checking again at iDup + 1
+        int result = 0;
+        int length = 0;
+
+
+        int i = 0;
+        while(i < s.length()) {
+            if(!map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), i);
+                length++;
+                i++;
+            }
+            else{   
+                result = Math.max(result, length);
+                length = 0;
+                i = map.get(s.charAt(i)) + 1;
+                map.clear();
+            }
+        }
+        result = Math.max(result, length);
+
+        return result;
+    }
+
+    // Given an integer array, finds the largest 'area' if each value
+    // was a line, and the width was diff between indices
+    public int maxArea(int[] height) {
+        int maxArea = 0, area = 0;
+
+        int left = 0, right = height.length - 1;
+
+        while(left < right) {
+            area = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, area);
+            if(height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return maxArea;
+    } 
+
+    // Converts given string into ZigZag format based on numRows
+    // Returns the string read row by row
+    // Works but is slow and uses a lot of memory
+    public String convert(String s, int numRows) {
+
+        if(numRows == 1)
+            return s;
+
+        StringBuilder result = new StringBuilder();
+        
+        char[][] ziggy = new char[numRows][s.length()];
+
+        int i = 0, row = 0, col = 0;
+        while(i < s.length()) {
+            if(row == 0) {
+                for(int j = 0; i < s.length() && j < numRows; j++) {
+                    ziggy[row++][col] = s.charAt(i++);
+                }
+                row--;
+                col++;
+            }
+            else{
+                ziggy[row][col++] = s.charAt(i++);
+            }
+            row--;
+        }
+
+        for(int j = 0; j < numRows; j++){
+            for(int k = 0; k < s.length(); k++) {
+                System.out.print(ziggy[j][k] + " ");
+                if(Character.isAlphabetic(ziggy[j][k]) || ziggy[j][k] == ',' || ziggy[j][k] == '.')
+                    result.append(ziggy[j][k]);
+            }
+            System.out.println("");
+        }
+
+        return result.toString();
+    }
+    // WIP, faster but not complete
+    public String convert2(String s, int numRows) {
+        StringBuilder result = new StringBuilder();
+
+        int cycleLength = 2*numRows - 2;
+
+        for(int i = 0; i < numRows; i++) {
+            for(int j = i; j < s.length(); j+= cycleLength) {
+                result.append(s.charAt(j));
+            }
+        }
+
+        return result.toString();
+    }
+
+    // Reverses the digits of a signed integer
+    public int reverse(int x) {
+        int result = 0, num = x;
+        boolean isNeg = false;
+
+        if(x < 0)
+            isNeg = true;
+
+        int exp = -1;
+
+        // Calculates '10' multiplier for reversal
+        while(num != 0){
+            exp++;
+            num /= 10;
+        }
+
+        num = x;
+
+        double multD = Math.pow(10, exp);
+        
+        int mult = (int)multD;
+
+        // Checks if overflow
+        if(mult >= 1000000000 && x%10 > 2)
+            return 0;
+
+        while(num != 0) {
+            result += (num%10) * mult;
+            num /= 10;
+            mult /= 10;
+        }
+
+        // Checks if overflow
+        if((isNeg && result > 0) || (!isNeg && result < 0))
+            return 0;
+        
+        return result;
+    }
+
+    // Converts string into integer
+    // Ignores whitespace and letters
+    public int myAtoi(String s) {
+        int result = 0, i = 0;
+        boolean isNeg = false, readDigits = false;
+        String noSpace = s.trim();
+        StringBuilder resString = new StringBuilder();
+
+        if(noSpace.isEmpty())
+            return 0;
+
+        if(noSpace.charAt(0) == '-') {
+            i++;
+            isNeg = true;
+        }
+        else if(noSpace.charAt(0) == '+')
+            i++;
+            
+        while(i < noSpace.length() && Character.isDigit(noSpace.charAt(i))){
+            // Checks if there is at least 1 digit
+            readDigits = true;
+            resString.append(noSpace.charAt(i++));
+        }
+
+        try {
+            result = Integer.parseInt(resString.toString());
+            if(isNeg)
+                return -1*result;
+            return result;
+        }
+
+        catch(Exception e) {
+            // readDigits verifies whether exception is because int is too big or no int read
+            if (readDigits){
+                if(isNeg)
+                    return Integer.MIN_VALUE;
+                else
+                    return Integer.MAX_VALUE;
+            }
+
+            return result;
+        }
+    }
+
+    // Finds the median value of two sorted arrays
+    // Can check when combined length is odd, not even (yet)
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+        // Cases when 1 array is empty
+        if(nums2.length == 0) {
+            int length = nums1.length;
+            if(length % 2 == 1)
+                return nums1[(int)Math.floor(length / 2)];
+            else
+                return (nums1[length/2] + nums1[(length/2) - 1])/2;
+        }
+        else if (nums1.length == 0){
+            int length = nums2.length;
+            if(length % 2 == 1)
+                return nums2[(int)Math.floor(length / 2)];
+            else
+                return (nums2[length/2] + nums2[(length/2) - 1])/2;
+        }
+
+        double totalLength = nums1.length + nums2.length;
+        int result = 0;
+
+        // length is odd
+        if(totalLength % 2 == 1) {
+
+            int index = (int)Math.floor(totalLength/2);
+            int count = 0, i = 0, j = 0;
+            while(count != index+1) {
+                if(nums1[i] < nums2[j]) {
+                    result = nums1[i++];
+                    count++;
+                }
+                else{
+                    result = nums2[j++];
+                    count++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        public ListNode create(int[] values) {
+            ListNode result = null;
+
+            return result;
+        }
+    }
+
+    // Adds two numbers where each digit is a node in a linked list
+    // Head is least significant digit
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode();
+        ListNode tracker = result;
+
+        int val3, carry = 0;
+
+        while(l1 != null && l2 != null) {
+            val3 = (l1.val + l2.val + carry) % 10;
+            // Carry used for next add
+            carry = (l1.val + l2.val + carry)/10;
+
+            tracker.next = new ListNode(val3);
+            tracker = tracker.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while(l1 != null) {
+            val3 = (l1.val + carry) % 10;
+            carry = (l1.val + carry) / 10;
+
+            tracker.next = new ListNode(val3);
+            tracker = tracker.next;
+            l1 = l1.next;
+        }
+
+        while(l2 != null) {
+            val3 = (l2.val + carry) % 10;
+            carry = (l2.val + carry) / 10;
+
+            tracker.next = new ListNode(val3);
+            tracker = tracker.next;
+            l2 = l2.next;
+        }
+
+        if(carry != 0)
+            tracker.next = new ListNode(carry);
+
+
+        return result.next;
+    }
+
+}
---
+++ tesingLC.java
@@ -0,0 +1,109 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+
+public class tesingLC {
+
+    @Test
+    public void testConvert(){
+        Leetcode lcTest = new Leetcode();
+
+        String s = "PAYPALISHIRING";
+
+        assertEquals("Test 1 row", "PAYPALISHIRING", lcTest.convert2(s, 1));
+
+        assertEquals("Test 3 rows", "PAHNAPLSIIGYIR", lcTest.convert2(s, 3));
+
+        assertEquals("Test 4 rows", "PINALSIGYAHRPI", lcTest.convert2(s, 4));
+
+    }
+
+    // Test cases not in use
+    public static void main(String args[]) {
+        // Testing reverse method
+        /*System.out.println(lcTest.reverse(-1234));
+        System.out.println(lcTest.reverse(1));
+        System.out.println(lcTest.reverse(1534236469));
+        System.out.println(lcTest.reverse(2147483647)); */
+
+        // Testing atoi method
+        /*System.out.println(lcTest.myAtoi("42"));
+        System.out.println(lcTest.myAtoi("  -42"));
+        System.out.println(lcTest.myAtoi(" 4193 with words"));
+        System.out.println(lcTest.myAtoi("-91283472332"));
+        System.out.println(lcTest.myAtoi("91283472332")); */
+
+        // Testing median method
+        /*int[] a1 = {1, 2}, a2 = {};
+        System.out.println(lcTest.findMedianSortedArrays(a1, a2)); */
+
+        // Testing addTwoNumbers method
+        /*Leetcode.ListNode l1 = lcTest.new ListNode(2, lcTest.new ListNode(4, lcTest.new ListNode(3, null)));
+        Leetcode.ListNode l2 = lcTest.new ListNode(5, lcTest.new ListNode(6, lcTest.new ListNode(4, null)));
+        System.out.println(lcTest.addTwoNumbers(l1, l2).next.next.val); */
+
+        // Testing longestSubstring method
+        /*String s1 = "abcdefg";
+        String s2 = "abcabcbb";
+        String s3 = "bbb";
+        System.out.println(lcTest.lengthOfLongestSubstring(s1));
+        System.out.println(lcTest.lengthOfLongestSubstring(s2));
+        System.out.println(lcTest.lengthOfLongestSubstring(s3)); */
+
+        // Testing maxArea
+        /*int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(lcTest.maxArea(height)); */
+
+        // Testing validSudoku method
+        /* char[][] board = {{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        System.out.println(lcTest.isValidSudoku(board)); */
+
+        // Testing plusOne method
+        /*int[] a1 = {1, 2, 3};
+        int[] a2 = {0};
+        int[] a3 = {9, 9, 9};
+        int[] a4 = {1, 2, 9};
+        System.out.println(Arrays.toString(lcTest.plusOne(a1)));
+        System.out.println(Arrays.toString(lcTest.plusOne(a2)));
+        System.out.println(Arrays.toString(lcTest.plusOne(a3)));
+        System.out.println(Arrays.toString(lcTest.plusOne(a4))); */
+    }
+
+    // Works but VERY slow
+    public int lengthOfLongestSubstring(String s) {
+        
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>(s.length());
+        // Store char and key in hashmap, if dupe found, start checking again at iDup + 1
+        int result = 0;
+        int length = 0;
+
+
+        int i = 0;
+        while(i < s.length()) {
+            if(!map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), i);
+                length++;
+                i++;
+            }
+            else{   
+                result = Math.max(result, length);
+                length = 0;
+                i = map.get(s.charAt(i)) + 1;
+                map.clear();
+            }
+        }
+        result = Math.max(result, length);
+
+        return result;
+    }
+
+}
